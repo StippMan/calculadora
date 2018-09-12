ff590cbbae99072202935741d4261f40264c22be
StippMan <ra102919@uem.br>
2018-09-12 16:37:45 -0300

    main done

--- main.c
+++ main.c
@@ -17,5 +17,15 @@ double div(double x, double y){
 }
 
 void main(){
+	double x, y;
+	printf("X: ");
+	scanf(" %lf", &x);
+	printf("Y: ");
+	scanf(" %lf", &y);
+
+	printf("Soma: %lf\n", soma(x,y));
+	printf("Subtracao: %lf\n", sub(x,y));
+	printf("Multiplicacao: %lf\n", mul(x,y));
+	printf("Divisao: %lf\n", div(x,y));
 
 }
